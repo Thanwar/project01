05f351355e9ae99b8ec781b0ab6de9e8476a4c37
Thanwar <60439635+Thanwar@users.noreply.github.com>
2020-12-03 18:53:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <h1>Hello World</h1>
-          <h2>updated by workflow</h2>
+          <h2>updated by workflow2.o</h2>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
